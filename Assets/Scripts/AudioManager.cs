83a64e96e378496eddbddf071bbb166748835e7d
Gleb Dolgoborodov <glebdolgoborodov@yandex.ru>
2025-02-04 18:14:24 +0300

    1

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource backgroundMusic;
+    [SerializeField] private AudioSource soundEffect1;
+    [SerializeField] private AudioSource soundEffect2;
+    [SerializeField] private AudioSource soundEffect3;
+    [SerializeField] private Toggle muteToggle;
+
+    private void Start()
+    {
+        muteToggle.onValueChanged.AddListener(ToggleAllSounds);
+    }
+
+    private void ToggleAllSounds(bool isMuted)
+    {
+        backgroundMusic.mute = isMuted;
+        soundEffect1.mute = isMuted;
+        soundEffect2.mute = isMuted;
+        soundEffect3.mute = isMuted;
+    }
+}
